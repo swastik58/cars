27643648893fec155e65aeef43497d9c6a6a43e0
swastik58 <swastiksupakar19@gmail.com>
2023-06-21 20:55:17 +0530

    initial commit

---
+++ utils/index.ts
@@ -0,0 +1,72 @@
+import { CarProps, FilterProps } from "@/types";
+
+export async function fetchCars(filters: FilterProps) {
+
+  const {manufacturer, year, model, limit, fuel} = filters;
+
+    const headers = {
+            'X-RapidAPI-Key': 'ad6798d867msh961b95801cbf5a4p164971jsn87c08eda2ce6',
+            'X-RapidAPI-Host': 'cars-by-api-ninjas.p.rapidapi.com'
+    }
+
+    const response = await fetch(`https://cars-by-api-ninjas.p.rapidapi.com/v1/cars?make=${manufacturer}&year=${year}&model=${model}&limit=${limit}&fuel_type=${fuel}`, {
+        headers: headers,
+    })
+
+    const result = await response.json();
+
+    return result;
+}
+
+
+export const calculateCarRent = (city_mpg: number, year: number) => {
+    const basePricePerDay = 50; // Base rental price per day in dollars
+    const mileageFactor = 0.1; // Additional rate per mile driven
+    const ageFactor = 0.05; // Additional rate per year of vehicle age
+  
+    // Calculate additional rate based on mileage and age
+    const mileageRate = city_mpg * mileageFactor;
+    const ageRate = (new Date().getFullYear() - year) * ageFactor;
+  
+    // Calculate total rental rate per day
+    const rentalRatePerDay = basePricePerDay + mileageRate + ageRate;
+  
+    return rentalRatePerDay.toFixed(0);
+  };
+
+
+  export const generateCarImageUrl = (car: CarProps, angle? : string) => {
+    const url = new URL('https://cdn.imagin.studio/getimage');
+
+    const {make, year, model} = car;
+
+    url.searchParams.append('customer', 'hrjavascript-mastery');
+
+    url.searchParams.append('make', make);
+    url.searchParams.append('modelFamily', model.split(' ')[0]);
+    url.searchParams.append('zoomType', 'fullscreen');
+    url.searchParams.append('modelYear', `${year}`);
+    url.searchParams.append('angle', `${angle}`);
+
+    return `${url}`;
+
+  }
+
+  export const updateSearchParams = (type:string, value: string) => {
+
+    //get current url for search params
+    const searchParams = new URLSearchParams(window.location.search);
+
+    //set specified search parameter to the given value
+    searchParams.set(type, value);
+
+    //set specified search parameter to the url
+    const newPathName = `${window.location.pathname}?${searchParams.toString()}`
+
+    return newPathName;
+  }
+
+  export interface ShowMoreProps {
+    pageNumber: number;
+    isNext: boolean;
+  }
